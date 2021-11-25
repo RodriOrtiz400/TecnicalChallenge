aa8cf2218bde24f3663040b056f0e9079121d358
Rodri Ortiz <rodrigo.ortiz@esolutions.com.ar>
2021-11-25 02:26:00 -0300

    Implementacion Sweet Alert

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,54 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AirlineService } from '../shared/services/airline/airline.service';
+import { AirlineService } from '../services/airline/airline.service';
 
-import { AuthService } from '../shared/services/auth/auth.service';
+import Swal from 'sweetalert2';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-
   authForm: FormGroup;
   public airlines: any[] = [];
- 
+
   constructor(
     private formBuilder: FormBuilder,
     private authLogin: AuthService,
     private airline: AirlineService,
     private router: Router
-  ) { 
+  ) {
     this.authForm = this.formBuilder.group({
       airline: ['HAWAIIAN AIRLINES (HA)'],
-      username: ['', [ Validators.required, Validators.minLength(4) ]],  
-      password: ['', [ Validators.required, Validators.minLength(4) ]]
-    })
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    });
   }
 
   ngOnInit(): void {
     this.airline.getAirlines().subscribe((airlineNames) => {
-     this.airlines = airlineNames     
-    })
+      this.airlines = airlineNames;
+    });
   }
 
   get usernameValid() {
-    return this.authForm.get('username')?.invalid && this.authForm.get('username')?.touched
+    return (
+      this.authForm.get('username')?.invalid &&
+      this.authForm.get('username')?.touched
+    );
   }
 
   get passwordValid() {
-    return this.authForm.get('username')?.invalid && this.authForm.get('username')?.touched
+    return (
+      this.authForm.get('username')?.invalid &&
+      this.authForm.get('username')?.touched
+    );
   }
 
-  login(){
+  login() {
     const values = this.authForm.value;
-    console.log(values);
-    
     this.authLogin.login(values.username, values.password).subscribe(
-      () => this.router.navigateByUrl('/home')
-    )
+      (res) => {
+        if (res.member.id) {
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            this.router.navigateByUrl('/home');
+          }        
+      },
+      (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Hubo un error de Usuario - Contraseña',
+        });
+      }
+    );
   }
-
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -6,17 +6,13 @@ import { Observable, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private httpClient: HttpClient) {}
+  constructor( private httpClient: HttpClient ) {}
 
   login(username: string, password: string): Observable<any> {
     const url = 'https://beta.id90travel.com/session.json';
     return this.httpClient.post<any>(url, { username, password }).pipe(
       tap((res) => {
         if (res.member.id) sessionStorage.setItem('id', res.member.id);
-          else {
-            //Realizar el manejo del error en caso de Error de Credenciales
-            
-          }
       })
     );
   }

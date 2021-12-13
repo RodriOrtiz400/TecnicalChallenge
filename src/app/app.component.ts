baacd612294f2f1d7678766bd044c74639b759a3
Rodri Ortiz <rodrigo.ortiz@esolutions.com.ar>
2021-12-13 09:05:03 -0300

    * Se incorpora GA4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare var gtag: any;
 
 @Component({
-  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  selector: 'app-root',
 })
 export class AppComponent {
   title = 'technicalChallenge';
+  constructor(private router: Router) {
+    const navEndEvents$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+
+//@ts-ignore
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-QS44KFVY28', {
+        'page_path': event.urlAfterRedirects,
+      });
+    });
+  } 
 }
